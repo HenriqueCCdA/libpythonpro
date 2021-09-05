b3b32a8e4d8ba0ce6ab98a173ed8c7f959c186a7
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2021-09-05 19:10:10 -0300

    Criar o teste quantidade de spam enviados
    close #21

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,5 +1,8 @@
+import pytest
+
 from libpythonpro.spam.enviador_de_email import Enviador
 from libpythonpro.spam.main import EnviadorDeSpam
+from libpythonpro.spam.modelos import Usuario
 
 
 def test_envio_de_spam(sessao):
@@ -7,3 +10,27 @@ def test_envio_de_spam(sessao):
     enviador_de_spam.enviar_emails('renzo@python.pro.br',
                                    'Curso Python Pro',
                                    'Confira os módulos fantáticos')
+
+
+@pytest.mark.parametrize(
+    'usuarios',
+    [
+        [
+            Usuario(nome='Renzo', email='renzo@python.pro.br'),
+            Usuario(nome='Luciano', email='luciano@python.pro.br')
+        ],
+        [
+            Usuario(nome='Renzo', email='renzo@python.pro.br')
+        ]
+    ]
+)
+def test_qte_de_spam_enviados(sessao, usuarios):
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    enviador = Enviador()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails('renzo@python.pro.br',
+                                   'Curso Python Pro',
+                                   'Confira os módulos fantáticos')
+
+    assert len(usuarios) == enviador.qtd_email_enviados
